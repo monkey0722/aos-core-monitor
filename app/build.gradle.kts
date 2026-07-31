37eec39b3be6861e90ab01ccff5ae006149217bc
monkey0722 <monkeys7222@gmail.com>
2026-07-31 10:35:26 +0900

    build: update targetSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
     defaultConfig {
         applicationId = "com.aoscoremonitor"
         minSdk = 33
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
